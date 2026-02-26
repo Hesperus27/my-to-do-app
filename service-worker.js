208218138714c5dc8c381b6dfe95b3564875531d
Hesperus27 <kentracy367@gmail.com>
2026-02-27 01:26:08 +0800

    Update service-worker.js
    Yes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "todo-pwa-v1";
+const CACHE_NAME = "todo-pwa-v2";
+
 const urlsToCache = [
   "./",
   "./index.html",
@@ -7,9 +8,21 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
   );
 });
 
